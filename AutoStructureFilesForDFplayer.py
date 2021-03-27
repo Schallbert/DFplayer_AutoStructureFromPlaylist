5aa301b8563012e5ce4e3b6076232ba19a106022
Schallbert <schal@mailbox.org>
2021-03-27 18:00:22 +0100

    Fix special char (accents) issue in filenames by escaping.

--- AutoStructureFilesForDFplayer.py
+++ AutoStructureFilesForDFplayer.py
@@ -3,7 +3,8 @@ import sys
 import os.path
 import shutil
 
-#Author: "Schallbert"
+
+# Author: "Schallbert"
 
 # this function pads leading zeros to a number, returning a string.
 def playerFSnumbers(indx, maxCnt):
@@ -13,39 +14,104 @@ def playerFSnumbers(indx, maxCnt):
         quitWithMessage("Error: player cannot process more than 100 folders")
     return str(indx).zfill(len(str(maxCnt)))
 
-def replaceSpecialCharactersInPlaylist(lineOfPlaylist):
+
+def replaceSpecialCharactersInPlaylist(line_of_playlist):
     """Make sure .m3u file's special characters are converted
-    Replace most commont HTML exclamations"""
-    lineOfPlaylist = lineOfPlaylist.replace("%20", " ")
-    lineOfPlaylist = lineOfPlaylist.replace("%21", "!")
-    lineOfPlaylist = lineOfPlaylist.replace("%22", '"')
-    lineOfPlaylist = lineOfPlaylist.replace("%23", "#")
-    lineOfPlaylist = lineOfPlaylist.replace("%24", "$")
-    lineOfPlaylist = lineOfPlaylist.replace("%25", "%")
-    lineOfPlaylist = lineOfPlaylist.replace("%26", "&")
-    lineOfPlaylist = lineOfPlaylist.replace("%27", "'")
-    lineOfPlaylist = lineOfPlaylist.replace("%28", "(")
-    lineOfPlaylist = lineOfPlaylist.replace("%29", ")")
-    lineOfPlaylist = lineOfPlaylist.replace("%2A", "*")
-    lineOfPlaylist = lineOfPlaylist.replace("%2B", "+")
-    lineOfPlaylist = lineOfPlaylist.replace("%2C", ",")
-    lineOfPlaylist = lineOfPlaylist.replace("%2D", "-")
-    lineOfPlaylist = lineOfPlaylist.replace("%2E", ".")
-    lineOfPlaylist = lineOfPlaylist.replace("%2F", "/")
-    lineOfPlaylist = lineOfPlaylist.replace("%3A", ":")
-    lineOfPlaylist = lineOfPlaylist.replace("%3B", ";")
-    lineOfPlaylist = lineOfPlaylist.replace("%3C", "<")
-    lineOfPlaylist = lineOfPlaylist.replace("%3D", "=")
-    lineOfPlaylist = lineOfPlaylist.replace("%3E", ">")
-    lineOfPlaylist = lineOfPlaylist.replace("%3F", "?")
-    lineOfPlaylist = lineOfPlaylist.replace("%7E", "~")  
-    return lineOfPlaylist
+    Replace most commont UTF8 special characters"""
+    line_of_playlist = line_of_playlist.replace("%20", " ")
+    line_of_playlist = line_of_playlist.replace("%21", "!")
+    line_of_playlist = line_of_playlist.replace("%22", '"')
+    line_of_playlist = line_of_playlist.replace("%23", "#")
+    line_of_playlist = line_of_playlist.replace("%24", "$")
+    line_of_playlist = line_of_playlist.replace("%25", "%")
+    line_of_playlist = line_of_playlist.replace("%26", "&")
+    line_of_playlist = line_of_playlist.replace("%27", "'")
+    line_of_playlist = line_of_playlist.replace("%28", "(")
+    line_of_playlist = line_of_playlist.replace("%29", ")")
+    line_of_playlist = line_of_playlist.replace("%2A", "*")
+    line_of_playlist = line_of_playlist.replace("%2B", "+")
+    line_of_playlist = line_of_playlist.replace("%2C", ",")
+    line_of_playlist = line_of_playlist.replace("%2D", "-")
+    line_of_playlist = line_of_playlist.replace("%2E", ".")
+    line_of_playlist = line_of_playlist.replace("%2F", "/")
+    line_of_playlist = line_of_playlist.replace("%3A", ":")
+    line_of_playlist = line_of_playlist.replace("%3B", ";")
+    line_of_playlist = line_of_playlist.replace("%3C", "<")
+    line_of_playlist = line_of_playlist.replace("%3D", "=")
+    line_of_playlist = line_of_playlist.replace("%3E", ">")
+    line_of_playlist = line_of_playlist.replace("%3F", "?")
+    line_of_playlist = line_of_playlist.replace("%7E", "~")
+    line_of_playlist = line_of_playlist.replace("%C3%80", "À")
+    line_of_playlist = line_of_playlist.replace("%C3%81", "Á")
+    line_of_playlist = line_of_playlist.replace("%C3%82", "Â")
+    line_of_playlist = line_of_playlist.replace("%C3%83", "Ã")
+    line_of_playlist = line_of_playlist.replace("%C3%84", "Ä")
+    line_of_playlist = line_of_playlist.replace("%C3%85", "Å")
+    line_of_playlist = line_of_playlist.replace("%C3%86", "Æ")
+    line_of_playlist = line_of_playlist.replace("%C3%87", "Ç")
+    line_of_playlist = line_of_playlist.replace("%C3%88", "È")
+    line_of_playlist = line_of_playlist.replace("%C3%89", "É")
+    line_of_playlist = line_of_playlist.replace("%C3%8A", "Ê")
+    line_of_playlist = line_of_playlist.replace("%C3%8B", "Ë")
+    line_of_playlist = line_of_playlist.replace("%C3%8C", "Ì")
+    line_of_playlist = line_of_playlist.replace("%C3%8D", "Í")
+    line_of_playlist = line_of_playlist.replace("%C3%8E", "Î")
+    line_of_playlist = line_of_playlist.replace("%C3%8F", "Ï")
+    line_of_playlist = line_of_playlist.replace("%C3%90", "Ð")
+    line_of_playlist = line_of_playlist.replace("%C3%91", "Ñ")
+    line_of_playlist = line_of_playlist.replace("%C3%92", "Ò")
+    line_of_playlist = line_of_playlist.replace("%C3%93", "Ó")
+    line_of_playlist = line_of_playlist.replace("%C3%94", "Ô")
+    line_of_playlist = line_of_playlist.replace("%C3%95", "Õ")
+    line_of_playlist = line_of_playlist.replace("%C3%96", "Ö")
+    line_of_playlist = line_of_playlist.replace("%C3%98", "Ø")
+    line_of_playlist = line_of_playlist.replace("%C3%99", "Ù")
+    line_of_playlist = line_of_playlist.replace("%C3%9A", "Ú")
+    line_of_playlist = line_of_playlist.replace("%C3%9B", "Û")
+    line_of_playlist = line_of_playlist.replace("%C3%9C", "Ü")
+    line_of_playlist = line_of_playlist.replace("%C3%9D", "Ý")
+    line_of_playlist = line_of_playlist.replace("%C3%9E", "Þ")
+    line_of_playlist = line_of_playlist.replace("%C3%9F", "ß")
+    line_of_playlist = line_of_playlist.replace("%C3%A0", "à")
+    line_of_playlist = line_of_playlist.replace("%C3%A1", "á")
+    line_of_playlist = line_of_playlist.replace("%C3%A2", "â")
+    line_of_playlist = line_of_playlist.replace("%C3%A3", "ã")
+    line_of_playlist = line_of_playlist.replace("%C3%A4", "ä")
+    line_of_playlist = line_of_playlist.replace("%C3%A5", "å")
+    line_of_playlist = line_of_playlist.replace("%C3%A6", "æ")
+    line_of_playlist = line_of_playlist.replace("%C3%A7", "ç")
+    line_of_playlist = line_of_playlist.replace("%C3%A8", "è")
+    line_of_playlist = line_of_playlist.replace("%C3%A9", "é")
+    line_of_playlist = line_of_playlist.replace("%C3%AA", "ê")
+    line_of_playlist = line_of_playlist.replace("%C3%AB", "ë")
+    line_of_playlist = line_of_playlist.replace("%C3%AC", "ì")
+    line_of_playlist = line_of_playlist.replace("%C3%AD", "í")
+    line_of_playlist = line_of_playlist.replace("%C3%AE", "î")
+    line_of_playlist = line_of_playlist.replace("%C3%AF", "ï")
+    line_of_playlist = line_of_playlist.replace("%C3%B0", "ð")
+    line_of_playlist = line_of_playlist.replace("%C3%B1", "ñ")
+    line_of_playlist = line_of_playlist.replace("%C3%B2", "ò")
+    line_of_playlist = line_of_playlist.replace("%C3%B3", "ó")
+    line_of_playlist = line_of_playlist.replace("%C3%B4", "ô")
+    line_of_playlist = line_of_playlist.replace("%C3%B5", "õ")
+    line_of_playlist = line_of_playlist.replace("%C3%B6", "ö")
+    line_of_playlist = line_of_playlist.replace("%C3%B8", "ø")
+    line_of_playlist = line_of_playlist.replace("%C3%B9", "ù")
+    line_of_playlist = line_of_playlist.replace("%C3%BA", "ú")
+    line_of_playlist = line_of_playlist.replace("%C3%BB", "û")
+    line_of_playlist = line_of_playlist.replace("%C3%BC", "ü")
+    line_of_playlist = line_of_playlist.replace("%C3%BD", "ý")
+    line_of_playlist = line_of_playlist.replace("%C3%BE", "þ")
+    line_of_playlist = line_of_playlist.replace("%C3%BF", "ÿ")
+    return line_of_playlist
+
 
 def quitWithMessage(messageString):
     """Quits script gracefully with message"""
     print(messageString)
     input("Press Enter to quit...")
     sys.exit()
+
 
 print("\
 Starting Conversion script for structuring files for DFplayerMini.\n\
@@ -160,8 +226,10 @@ if listOfPlaylists:
                     shutil.copy2(line, os.path.join(os.sep, tgtFldrPath,
                                                     tgtFileName))  # copies & renames file from playlist to tgt folder
                 except:
-                    quitWithMessage("--- ERROR --- Couldn't copy file\n" + str(line) + " to:\n" + str(tgtFldrPath) + "\nAborting.")
-        print("...completed for folder %d of %d: %s with %d files" % (folderIndex, len(listOfPlaylists), item, fileIndex))
+                    quitWithMessage(
+                        "--- ERROR --- Couldn't copy file\n" + str(line) + " to:\n" + str(tgtFldrPath) + "\nAborting.")
+        print(
+            "...completed for folder %d of %d: %s with %d files" % (folderIndex, len(listOfPlaylists), item, fileIndex))
     quitWithMessage("--- SUCCESS --- All actions completed successfully.")
 else:
     quitWithMessage("--- ERROR --- No playlists found in folder. Place .m3u file(s) here, please. Aborting.")
