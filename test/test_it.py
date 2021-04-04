4bf32631a0017e8da6760189c7e15511f074e615
Schallbert <schal@mailbox.org>
2021-04-04 13:24:18 +0200

    add unittest file

---
+++ test/test_it.py
@@ -0,0 +1,11 @@
+#>>> os.path.isdir('new_folder')
+#True
+#>>> os.path.exists(os.path.join(os.getcwd(), 'new_folder', 'file.txt'))
+#False
+
+#from DFplayer_AutoStructureFromPlaylist.AutoStructureFilesForDFplayer import *
+
+
+#def test_answer():
+#    AutoStructure
+#    assert os.path.isdir(../)
