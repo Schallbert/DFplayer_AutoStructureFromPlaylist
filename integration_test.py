a3bc797d7c5be7d881cea33fed62c2676f3dc777
Schallbert <schal@mailbox.org>
2021-04-06 20:30:45 +0200

    add integration test cases

---
+++ integration_test.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+import pytest
+
+ROOTDIR = os.getcwd()
+TARGET = 'AutoStructureFilesForDFplayer.py'
+PLAYLISTFOLDER = 'PlayListsM3U'
+SDCARDFOLDER = 'SDcardFolders'
+DUMMYPLAYLISTS = ROOTDIR + '/test/m3u_source'
+DUMMYCONTENT = ROOTDIR + '/test/mp3_source'
+
+
+def execute_target(tmp_path):
+    os.chdir(tmp_path)
+    os.system(TARGET)
+
+
+# executest the script "Software Under Test"
+@pytest.fixture
+def copy_target_to_temp(tmp_path):
+    """places SUT in temporary path to prepare for execution"""
+    shutil.copyfile(ROOTDIR + "/" + TARGET, tmp_path / TARGET)
+
+
+@pytest.fixture
+def execute_happy_path(tmp_path, copy_target_to_temp):
+    execute_target(tmp_path)
+    copy_dummy_content_to_temp(tmp_path)
+    execute_target(tmp_path)
+
+
+def copy_dummy_content_to_temp(tmp_path):
+    """after execute_target is run, this function copies the dummy contents to the playlists and executes again"""
+    # Copy dummy m3u playlists
+    for files in os.listdir(DUMMYPLAYLISTS):
+        shutil.copy(DUMMYPLAYLISTS + "/" + files, tmp_path / PLAYLISTFOLDER)
+    # Copy dummy mp3 content
+    shutil.copytree(DUMMYCONTENT, tmp_path / 'mp3_source')
+
+
+def test_can_execute_target(tmp_path, copy_target_to_temp):
+    execute_target(tmp_path)
+    d = tmp_path / TARGET
+    assert d.exists()
+
+
+def test_creates_playlist_folder(tmp_path, copy_target_to_temp):
+    execute_target(tmp_path)
+    d = tmp_path / PLAYLISTFOLDER
+    assert d.exists()
+
+
+def test_creates_sdcard_folder(tmp_path, copy_target_to_temp):
+    execute_target(tmp_path)
+    d = tmp_path / SDCARDFOLDER
+    assert d.exists()
+
+
+def test_copies_playlists_to_folder(tmp_path, copy_target_to_temp):
+    execute_target(tmp_path)
+    copy_dummy_content_to_temp(tmp_path)
+    d = tmp_path / PLAYLISTFOLDER
+    cpyfiles = os.listdir(d)
+    srcfiles = os.listdir(DUMMYPLAYLISTS)
+    assert len(cpyfiles) == len(srcfiles)
+
+
+def test_creates_folders_from_dummy(tmp_path, execute_happy_path):
+    p = tmp_path / SDCARDFOLDER
+    d = p / '01'
+    assert d.exists()
+    d = p / '02'
+    assert d.exists()
+    d = p / '03'
+    assert d.exists()
+
+
+def test_creates_files_from_dummy(tmp_path, execute_happy_path):
+    p = tmp_path / SDCARDFOLDER
+    d = p / '01'
+    assert len(os.listdir(d)) == 7
+    d = p / '02'
+    assert len(os.listdir(d)) == 7
+    d = p / '03'
+    assert len(os.listdir(d)) == 7
+
+# def test_creates_correct_filenames(tmp_path):
+#   execute_happy_path(tmp_path)
+#  for w in range(1, 4):
+#     p = tmp_path / SDCARDFOLDER / ('0' + str(w))
+#    for x in range(1, 8):
+#       d = p / ('00' + str(x))
+#      assert d.exists()
